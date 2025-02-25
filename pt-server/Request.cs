b01621d4030714fe4db3203cb7365de670a9e2be
Marvin Schmalzbauer <e12215883@student.tuwien.ac.at>
2025-02-26 00:30:57 +0100

    Setup basic server

---
+++ pt-server/Request.cs
@@ -0,0 +1,64 @@
+using System.Text;
+
+class Request{
+    public enum Method
+    {
+        GET,
+        POST,
+        PUT,
+        DELETE
+    }
+    private string path = "";
+    private string pathHttp = "";
+    private Dictionary<string,string> headers = new Dictionary<string, string>();
+    private Method method;
+    public Request(string request){
+        string[] parts = request.Split("\r\n");
+        ParseRequest(parts);
+    }
+    
+    private void ParseRequest(string[] request){
+        string[] method_split = request[0].Split(' ');
+        
+        switch (method_split[0])
+        {
+            case "GET":
+            method = Method.GET;
+            break;
+            case "PUT":
+            method = Method.PUT;
+            break;
+            case "POST":
+            method = Method.POST;
+            break;
+            case "DELETE":
+            method = Method.DELETE;
+            break;
+            default:
+            break;
+        }
+        path = method_split[1];
+        pathHttp = method_split[2];
+        for (int i = 1; i < request.Length; i++)
+        {
+            if(String.IsNullOrEmpty(request[i]))
+                break;
+            System.Console.WriteLine(request[i] + " i: "+ i);
+            var line = request[i].Split(": ");
+            headers.Add(line[0],line[1]);
+        }
+    }
+    public override string ToString()
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.Append("Method = ").Append(method).AppendLine();
+        sb.Append("Path = ").Append(path).AppendLine();
+        sb.Append("HTTP = ").Append(pathHttp).AppendLine();
+        foreach (var item in headers)
+        {
+            sb.Append(item.Key).Append(": ").Append(item.Value).AppendLine();
+        }
+        return sb.ToString();
+    }
+    
+}
---
+++ pt-server/Server.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Server
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpListener tcpListener = new TcpListener(IPAddress.Parse("127.0.0.1"),8008);
+            tcpListener.Start();
+            TcpClient tcpClient = new TcpClient();
+            byte[] buffer = new byte[0];
+            while(true){
+                buffer = new byte[1024];
+                tcpClient = tcpListener.AcceptTcpClient();
+                var cStream = tcpClient.GetStream();
+                cStream.Read(buffer);
+                Request request = new Request(System.Text.Encoding.Default.GetString(buffer));
+                System.Console.WriteLine(request);
+            }
+        }
+    }
+}
